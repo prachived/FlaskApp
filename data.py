0a3dd49dc21fe3519cf141bc57de71c37d5f84b1
Prachi Ved <prachived26@gmail.com>
2019-01-10 10:28:54 -0500

    Implementing Articles section

---
+++ data.py
@@ -0,0 +1,29 @@
+def Articles():
+    articles = [
+        {
+            'id': 1,
+            'title':'Article One',
+            'body': 'Demo article 1 body content',
+            'author': 'Prachi Ved',
+            'date_created': '01-10-2019'
+
+        },
+    {
+        'id': 2,
+        'title': 'Article Two',
+        'body': 'Demo article 2 body content',
+        'author': 'Prachi Ved',
+                  'date_created':'01-10-2019'
+
+    },
+    {
+        'id': 3,
+        'title': 'Article Three',
+        'body': 'Demo article 3 body content',
+        'author': 'Prachi Ved',
+                  'date_created':'01-10-2019'
+
+    }
+
+    ]
+    return articles
